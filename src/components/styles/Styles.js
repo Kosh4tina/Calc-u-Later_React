1614b1ff79967278af9961a36e94680410501212
Kosh4tina <Konstantsin.kot@gmail.com>
2022-06-27 15:02:34 +0200

    Updated Project

--- src/components/styles/Styles.js
+++ src/components/styles/Styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const CalculatorStyles = styled.div`
   background-color: #4abdac;
   max-width: 100%;
-  height: 100vh;
+  height: 100%;
   display: grid;
   justify-items: center;
   grid-template-areas:
